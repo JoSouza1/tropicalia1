5afbc35df9293b957cd98dc3093451164487ff77
JoSouza1 <141272660+JoSouza1@users.noreply.github.com>
2025-02-19 13:49:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFontBotao = document.getElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFontBotao.addEventListener('click', function(){
+        
+    })
+})
